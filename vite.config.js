b0f4bd896e7aa829e9007c444a0bca546fa88ea4
Tolga <cavga228@gmail.com>
2025-04-17 01:39:58 +0300

    changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 🟠 BU KISMI EKLE: GitHub repo adınla aynı olmalı
 export default defineConfig({
-  base: '/urun-listesi0/',
+  base: '/urun-listesi0/', 
   plugins: [react()],
 })
